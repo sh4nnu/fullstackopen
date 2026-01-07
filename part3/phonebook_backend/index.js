3be7c1662ba5883ffc05828f9b25da42c83f7cd2
Mani Kishan Ghantasala <manikishanghantasala@gmail.com>
2026-01-06 19:41:48 -0600

    add support for mongodb for backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 var morgan = require('morgan')
 
 const app = express()
 
-const PORT = 3001
+const PORT = process.env.PORT
+const Person = require('./models/person')
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
@@ -53,42 +57,52 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    Person.countDocuments().then(count => {
+        res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(id).then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    }).catch(error => {
+        next(error)
+    })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    Person.findByIdAndDelete(id).then(() => {
+        res.status(204).end()
+    }).catch(error => {
+        next(error)
+    })
 })
 
 
 
 
-const generateId = () => {
-    let randomID = Math.floor(Math.random() * 10000)
-    while (persons.some(person => person.id === randomID.toString())) {
-        randomID = Math.floor(Math.random() * 10000)
-    }
-    return randomID.toString()
-}
+// const generateId = () => {
+//     let randomID = Math.floor(Math.random() * 10000)
+//     while (persons.some(person => person.id === randomID.toString())) {
+//         randomID = Math.floor(Math.random() * 10000)
+//     }
+//     return randomID.toString()
+// }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
     if (!body.name || !body.number) {
         return res.status(400).json({
@@ -100,14 +114,46 @@ app.post('/api/persons', (req, res) => {
             error: 'name must be unique'
         })
     }
-    const newPerson = {
-        id: generateId(),
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number
+    })
+    newPerson.save().then(savedPerson => {
+        res.json(savedPerson)
+    }).catch(error => {
+        next(error)
+    })
+    
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id
+    const body = req.body
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    const updatedPerson = {
         name: body.name,
         number: body.number
     }
-    persons = persons.concat(newPerson)
-    res.json(newPerson)
+    Person.findByIdAndUpdate(id, updatedPerson, { new: true }).then(result => {
+        res.json(result)
+    }).catch(error => {
+        next(error)
+    })
 })
+
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'unknown endpoint' })
