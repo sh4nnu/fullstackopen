bb1b8df5ed3cc3bcf8a5ea83476cae1b43efc1f6
Mani Kishan Ghantasala <manikishanghantasala@gmail.com>
2026-01-16 14:02:35 -0600

    Add Togglable class and make blog form togglable

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -11,9 +13,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+  
   const [notifyMessage, setNotifyMessage] = useState(null)
   const [notifyColor, setNotifyColor] = useState(null)
 
@@ -69,23 +69,16 @@ const App = () => {
     setUser(null)
   }
 
-  const handleBlog = async (event) =>{
-    event.preventDefault()
-    try {
-      const newBlog = await blogService.create({
-        title,
-        author,
-        url,
-      })
-      setBlogs(blogs.concat(newBlog))
-      setTitle('')
-      setAuthor('')
-      setUrl('')
-      showNotification(`a new blog  ${newBlog.title}! by ${newBlog.author} added`, 'green')
-    } catch (error) {
-      showNotification(`failed to create blog: ${error}`, 'red')
+const addBlog = async (blogObject) =>{
+        try {
+        const newBlog = await blogService.create(blogObject)
+        setBlogs(blogs.concat(newBlog))
+        
+        showNotification(`a new blog  ${newBlog.title}! by ${newBlog.author} added`, 'green')
+        } catch (error) {
+        showNotification(`failed to create blog: ${error}`, 'red')
+        }
     }
-  }
 
   if (user === null) {
     return (
@@ -107,34 +100,9 @@ const App = () => {
       <Notification message={notifyMessage} color={notifyColor} />
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
-
-      <div>
-        <form onSubmit={handleBlog}>
-          <h2>create new</h2>
-          title: 
-          <input type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-        <br></br>
-        author: 
-          <input type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-        <br></br>
-        url: 
-          <input type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-        <br></br>
-        <button type="submit">create</button>
-        </form>
-      </div>
+      <Togglable buttonLabel="create new blog">
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+
+const BlogForm = ({createBlog}) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    const handleBlog = async (event) =>{
+        event.preventDefault()
+        createBlog({
+            title,
+            author,
+            url,
+        })
+        
+    }
+
+    return (
+     <>
+        <form onSubmit={handleBlog}>
+          <h2>create new</h2>
+          title: 
+          <input type="text"
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <br></br>
+        author: 
+          <input type="text"
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+        <br></br>
+        url: 
+          <input type="text"
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+        <br></br>
+        <button type="submit">create</button>
+        </form>
+      </>
+    )
+}
+export default BlogForm
